59970506d95e9316818d87a21c5c4431ddc9362a
francy1999 <francesco.ardizzoni@gmail.com>
2024-05-09 13:11:46 +0200

    commit test

--- main.cpp
+++ main.cpp
@@ -218,6 +218,8 @@ int main() {
         }
     }
 
+    //this is my first chenges for this project
+
     // Закрытие серверного сокета и соединения с базой данных
     close(server_socket);
     sqlite3_close(db);
